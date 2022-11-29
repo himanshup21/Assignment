53f7f503fae29f6cbcbb733feae954da550e8533
himanshup21 <85743773+himanshup21@users.noreply.github.com>
2022-11-29 19:58:17 +0530

    Add files via upload

---
+++ Q1_parallelised.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+int fermat_prime(int x,int y){
+
+    int modulus = 1;
+    for (int i=1; i<=x-1; i++)
+        modulus = (modulus * y%x)%x;
+    
+    return modulus;
+}
+
+
+int main(){
+
+    char st;
+    int cnt = 499;
+    int m;
+    double t_start, t_end;
+    t_start = omp_get_wtime();
+    # pragma omp parallel for private(st)
+        for (int n=2; n<=500; n++){
+            st = 'T';
+            for (int m = 2; m<n; m++){
+                if ( fermat_prime(n,m) != 1){
+                    cnt --;
+                    st = 'F';
+                    break;
+                }
+            }
+        if (st == 'T')
+            printf("%d\t",n);
+
+    }
+    t_end = omp_get_wtime();
+
+    printf("\n cnt: %d\n",cnt);
+    printf("Time required to find primes = %f seconds\n", t_end - t_start);
+    return 0;
+}
